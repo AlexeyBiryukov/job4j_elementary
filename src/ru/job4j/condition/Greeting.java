ccf89c9f449b8699a7aacd5ea789f1838056306d
Java <AlexeyBiryukov/job4j_elementary>
2020-06-16 20:10:24 +0300

    9.1. String [#303720]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java!";
+        System.out.println(idea);
+        idea = idea + " " + "But i am a newbie.";
+        int year = 2020;
+        System.out.println(idea);
+    }
+}
