a7d100e8748ad033ce20e520609d30b189becf7c
Java <AlexeyBiryukov/job4j_elementary>
2020-07-03 20:06:22 +0300

    5.4. Протеиновая диета. [#303763]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             month++;
             ivan *= 3;
             nik *= 2;
@@ -16,5 +16,6 @@ public class Fitness {
         calc(95, 90);
         calc(90, 95);
         calc(50, 90);
+        calc(90, 90);
     }
 }
