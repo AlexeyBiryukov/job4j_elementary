e9b34a2b88bd099a6b7c2ff9658e95cd3a033418
Java <AlexeyBiryukov/job4j_elementary>
2020-07-16 21:52:59 +0300

    6.2.2. Переставить элементы массива [#303703]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -2,6 +2,15 @@ package ru.job4j.array;
 
 public class SwitchArray {
 
+    public static int[] swap(int[] array, int source, int dest) {
+        for (int index = 0; index < array.length; index++) {
+            int temp = array[source];
+            array[source] = array[dest];
+            array[dest] = temp;
+        }
+        return array;
+    }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
---
+++ test/ru/job4j/array/SwitchArrayTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SwitchArrayTest {
+
+    @Test
+    public void swapOne() {
+        int[] input = {1, 2, 3, 4};
+        int[] expect = {4, 2, 3, 1};
+        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
+    public void swapTwo() {
+        int[] input = {5, 3, 2, 0};
+        int[] expect = {0, 5, 3, 2};
+        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
+    public void SwapThree() {
+        int[] input = {7, 6, 4, 2, 0};
+        int[] expect = {0, 7, 6, 4, 2};
+        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
+        assertThat(rsl, is(expect));
+    }
+}
