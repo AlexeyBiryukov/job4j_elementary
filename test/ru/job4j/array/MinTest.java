521171a695304a0b90abf59fe347b39a334900af
Java <AlexeyBiryukov/job4j_elementary>
2020-07-29 11:32:09 +0300

    6.5.1. Поиск минимального числа в массиве. [#303693]

---
+++ test/ru/job4j/array/MinTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MinTest {
+
+    @Test
+    public void whenFirstMin() {
+        assertThat(Min.findMin(new int[] {0, 5, 10}), is(0));
+    }
+
+    @Test
+    public void whenSecondMin() {
+        assertThat(Min.findMin(new int[] {10, 2, 5}), is(2));
+    }
+
+    @Test
+    public void whenLastMin() {
+        assertThat(Min.findMin(new int[] {10, 9, 1}), is(1));
+    }
+}
