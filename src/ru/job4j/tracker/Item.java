0fa14c4d04c6bd656650b5b4a644303c31478553
Java <AlexeyBiryukov/job4j_elementary>
2020-10-12 21:15:49 +0300

    2. Перегрузка конструктора. [#303867]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -2,16 +2,20 @@ package ru.job4j.tracker;
 
 public class Item {
 
+    private int id;
+    private String name;
+
     public Item() {
 
     }
 
     public Item(int id) {
-
+        this.id = id;
     }
 
     public Item(int id, String name) {
-
+        this.id = id;
+        this.name = name;
     }
 
     public static void main(String[] args) {
