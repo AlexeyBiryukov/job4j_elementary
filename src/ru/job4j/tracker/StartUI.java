b8ab697e78bc23036e3354850dd3157185fa9880
Java <AlexeyBiryukov/job4j_elementary>
2020-11-07 02:32:37 +0300

    6. Метод замены заявки. Tracker.replace [#303877]

--- src/ru/job4j/tracker/StartUI.java
+++ src/ru/job4j/tracker/StartUI.java
@@ -4,8 +4,13 @@ public class StartUI {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
         Item item1 = new Item();
+        Item item2 = new Item();
+
         item1.setName("Number #1");
+        item2.setName("Number #2");
+
         tracker.add(item1);
+        tracker.replace(1, item2);
 
         System.out.println(tracker.findById(1));
     }
