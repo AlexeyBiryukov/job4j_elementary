971575eff5ba3de0d32956a2e3fa0727059ca0be
Java <AlexeyBiryukov/job4j_elementary>
2021-01-17 22:52:26 +0300

    2. Scanner и чтение числа из консоли. [#303813]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -5,33 +5,25 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int matches = 11;
-        boolean run = true;
-        System.out.println("Добро пожаловать в игру 11! ");
-        System.out.println();
-        System.out.println("!!! ПРАВИЛА ИГРЫ !!! ");
-        System.out.println("1. На столе лежат 11 спичек.");
-        System.out.println("2. Два игрока по очереди берут от 1 до 3 спичек.");
-        System.out.println("3. Выигрывает тот, кто забрал последние спички.");
-        System.out.println();
-
-        while (run) {
-            while (run) {
-                for (int i = 0; i < matches; i++) {
-                    System.out.println("Возьмите от 1й до 3х спичек ");
-                    int select1 = Integer.valueOf(input.nextLine());
-                    if (select1 < 1 || select1 > 3) {
-                        System.out.println("Вы ввели неверное число");
-                        System.out.println("На столе " + matches + " спичек");
-                    } else if (select1 > 0 || select1 <= 3) {
-                        matches = matches - select1;
-                        System.out.println("На столе " + matches + " спичек");
-                        System.out.println("-------------------------------");
-                    } if (matches == 0) {
-                        run = false;
-                    }
-                }
+        System.out.println("Игра 11.");
+        boolean turn = true;
+        int count = 11;
+        while (count > 0) {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до 3 : ");
+            int matches = Integer.parseInt(input.nextLine());
+            turn = !turn;
+            if (matches < 1 || matches > 3) {
+                System.out.println("введено неверное число");
+            } else if (matches > 1 || matches <= 3) {
+                count = count - matches;
+                System.out.println("На столе осталось " + count + " спичек.");
             }
+        }
+        if (!turn) {
+            System.out.println("Выиграл первый игрок");
+        } else {
+            System.out.println("Выиграл второй игрок");
         }
     }
 }
